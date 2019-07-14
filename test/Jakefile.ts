d051151fe3e26220f079307a7adfef38b3938fc2
Mehrdad Reshadi <mreshadi@instartlogic.com>
2019-07-13 19:09:01 -0700

    release: 1.0.6 making sure the test task is only activated when building in local directory

--- test/Jakefile.ts
+++ test/Jakefile.ts
@@ -1,23 +1,27 @@
 import * as Fs from "fs";
 import * as Build from "../Build";
-import { GlobalTask, CreateMakeRelative } from "jakets/lib/Jakets";
+import { GlobalTask, CreateMakeRelative, IsWorkingDir, Task } from "jakets/lib/Jakets";
 import { ClientTscCompileTask } from "../lib/ClientTscCompileTask";
 
 const MakeRelative = CreateMakeRelative(__dirname);
 
-export const TestTask = GlobalTask(
-  "build_test",
-  [
-    Build.ServerTscTask.Compile("app", ["./app/App.ts"].map(MakeRelative)),
-    Build.ClientTscTask.Compile("app", ["app/App.ts"].map(MakeRelative)),
-    // new ClientTscCompileTask({ closure: {} }, "/client_opt").Compile("client_opt", ["app/App.ts"].map(MakeRelative))
-  ],
-  async function () {
-    let output = Fs.readFileSync("./build/compile/client/release/app/app.js");
-    if (!/[.]prototype[.].*console[.]log\("Hello"\)/.test(output.toString())) {
-      throw `Incorrect output: ${output}`;
-    } else {
-      require("../build/compile/client/release/app/app");
-    }
-  }
-);
+export const TestTask =
+  IsWorkingDir(__dirname + "/..")
+    ? GlobalTask(
+      "build_test",
+      [
+        Build.ServerTscTask.Compile("app", ["./app/App.ts"].map(MakeRelative)),
+        Build.ClientTscTask.Compile("app", ["app/App.ts"].map(MakeRelative)),
+        // new ClientTscCompileTask({ closure: {} }, "/client_opt").Compile("client_opt", ["app/App.ts"].map(MakeRelative))
+      ],
+      async function () {
+        let output = Fs.readFileSync("./build/compile/client/release/app/app.js");
+        if (!/[.]prototype[.].*console[.]log\("Hello"\)/.test(output.toString())) {
+          throw `Incorrect output: ${output}`;
+        } else {
+          require("../build/compile/client/release/app/app");
+        }
+      }
+    )
+    : Task("")
+  ;
